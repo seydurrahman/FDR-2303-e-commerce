3c8da0ecbfa702f8d376e7db085d29143e9add91
Mohammad Seydur Rahman <seydur.rahman82@gmail.com>
2024-07-15 05:53:41 +0600

    Banner responsive done

--- src/Component/HomeComponent/Banner/Banner.jsx
+++ src/Component/HomeComponent/Banner/Banner.jsx
@@ -45,9 +45,35 @@ const Banner = () => {
               
               </>
             
-          )
+          ),
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 575,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots:false,
+              }
+            }
+          ]
       };
-      console.log(currentSlide);
   return (
     <div>
         <div className="container">
