8a7a5d402481940568a84456c45478c589ccefd2
Mohammad Seydur Rahman <seydur.rahman82@gmail.com>
2024-07-15 05:01:41 +0600

    Navbar responsive done

--- src/Component/CommonComponent/Footer/FooterBottom.jsx
+++ src/Component/CommonComponent/Footer/FooterBottom.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Flex from "../Flex"
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaSquareInstagram } from "react-icons/fa6";
 
 const FooterBottom = () => {
   return (
-    <div>FooterBottom</div>
+    <>
+      <Flex className={" justify-between items-center"}>
+       <div className='py-10'>
+       <Flex className={"items-center gap-x-6"}>
+       <FaFacebookF/>
+        <FaLinkedin/>
+        <FaSquareInstagram/>
+       </Flex>
+       </div>
+       <div>
+        <p className='text-footer_text_color font-DMsans text-sm'>20 pcalfhowueiornio maiores, aliquid modi </p>
+       </div>
+      </Flex>
+    </>
   )
 }
 
--- src/Component/HomeComponent/Header/Header.jsx
+++ src/Component/HomeComponent/Header/Header.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "../../../assets/Logo.png"
 import Flex from '../../CommonComponent/Flex'
+import { FaBars } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
+
 const Header = () => {
+    const [isShow, setisShow]=useState(false)
+
+    // handleNavIcon functionality//
+    const HandleNavIcon=()=>{
+        setisShow(!isShow);
+    }
+
   return (
     <>
-    <div className=' py-8 bg-main_bg_color'>
+    <div className=' py-8 bg-main_bg_color px-5 px-sm-0'>
         <div className="container">
            <Flex className=' justify-between items-center'>
            <div>
@@ -13,7 +23,7 @@ const Header = () => {
                 </picture>
             </div>
             <div>
-                <Flex className={" gap-x-10"}>
+                <Flex className={`absolute flex-col sm:static sm:bg-main_bg_color sm:flex-row sm:gap-x-5 ${isShow ? "left-0 bg-green-200 w-full z-10 items-center top-[80px] transition-all":"left-[-100%] bg-gray-200 w-full z-10 items-center top-[80px]"}`}>
                     <li>
                         <a href="#" className="menuItem transition-all hover:text-main_font_color hover:font-bold">Home</a>
                     </li>
@@ -31,7 +41,9 @@ const Header = () => {
                     </li>
                 </Flex>
             </div>
-            <div></div>
+            <div className='cursor-pointer sm:hidden' onClick={HandleNavIcon}>
+                {isShow===true?<RxCross2/>:<FaBars/> }
+            </div>
            </Flex>
         </div>
     </div>
